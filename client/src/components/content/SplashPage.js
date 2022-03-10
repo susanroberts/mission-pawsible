c734f977539a2a6e81cd09e0269a7e8fc1daf4a6
susanroberts <sroberts517@gmail.com>
2022-03-10 12:55:16 -0500

    update splash page

--- client/src/components/content/SplashPage.js
+++ client/src/components/content/SplashPage.js
@@ -8,7 +8,8 @@ const SplashPage = props => {
     <div className="grid-x grid-padding-x">
       <div className="cell small-2" />
       <div className="cell small-8">
-        <h2 className="centered">A straightforward tracker for pet separation anxiety training</h2>
+        <h2 className="centered">A tracker for pet separation anxiety training</h2>
+        <p className="centered">This app was built as my capstone at Launch Academy. It uses NodeJS, ReactJS, ExpressJS, PostgreSQL and RESTful API. The code is available to view <a className="white" href="https://github.com/susanroberts/mission-pawsible" target="_blank">HERE</a> on GitHub.</p>
         <img src={panic_to_calm} className="opal-tile centered" />
       </div>
       <div className="cell small-2" />
